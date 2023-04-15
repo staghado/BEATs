9706b2e2748847a1b9da4fa8c979c6aa2821d3ba
staghado <taghadouinisaid@gmail.com>
2023-04-16 01:38:53 +0200

    remove max_time and target_sample_rate

--- DataModule.py
+++ DataModule.py
@@ -14,17 +14,14 @@ from pytorch_lightning import LightningDataModule
 
 
 class AudioDataset(Dataset):
-    def __init__(self, root_dir, data_frame, target_sample_rate, max_time, transform=None):
+    def __init__(self, root_dir, data_frame, num_samples, transform=None):
         self.root_dir = root_dir
         self.transform = transform
         self.data_frame = data_frame
 
         self.label_encoder = LabelEncoder()
         self.label_encoder.fit(self.data_frame["category"])
-        self.target_sample_rate = target_sample_rate
-        num_samples = target_sample_rate * max_time
         self.num_samples = num_samples
-
 
     def __len__(self):
         return len(self.data_frame)
@@ -119,8 +116,7 @@ class BirdDataModule(LightningDataModule):
         data_frame = None,
         batch_size: int = 8,
         split_ratio=0.8,
-        target_sample_rate=32000,
-        max_time=5,
+        num_samples=32000 * 5,
         transform=None,
         **kwargs
     ):
@@ -129,8 +125,7 @@ class BirdDataModule(LightningDataModule):
         self.data_frame = data_frame
         self.batch_size = batch_size
         self.split_ratio = split_ratio
-        self.target_sample_rate=target_sample_rate,
-        self.max_time=max_time,
+        self.num_samples=num_samples,
         self.transform = transform
 
         self.setup()
@@ -149,75 +144,14 @@ class BirdDataModule(LightningDataModule):
 
     def train_dataloader(self):
         train_df = AudioDataset(
-            root_dir=self.root_dir, data_frame=self.train_set, target_sample_rate=self.target_sample_rate, max_time=self.max_time, transform=self.transform
+            root_dir=self.root_dir, data_frame=self.train_set, num_samples=self.num_samples, transform=self.transform
         )
 
         return DataLoader(train_df, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
         val_df = AudioDataset(
-            root_dir=self.root_dir, data_frame=self.val_set, target_sample_rate=self.target_sample_rate, max_time=self.max_time, transform=self.transform
+            root_dir=self.root_dir, data_frame=self.val_set, num_samples=self.num_samples, transform=self.transform
         )
 
         return DataLoader(val_df, batch_size=self.batch_size, shuffle=False)
-
-# class BirdCLEFDataset(Dataset):
-#     def __init__(self, df, target_sample_rate, max_time, image_transforms=None):
-#         self.file_paths = df['file_path'].values
-#         self.labels = df['primary_label'].values
-#         self.target_sample_rate = target_sample_rate
-#         num_samples = target_sample_rate * max_time
-#         self.num_samples = num_samples
-#         self.image_transforms = image_transforms
-        
-#     def __len__(self):
-#         return len(self.file_paths)
-    
-#     def __getitem__(self, index):
-#         filepath = self.file_paths[index]
-#         audio, sample_rate = torchaudio.load(filepath)
-#         audio = self.to_mono(audio)
-        
-#         if sample_rate != self.target_sample_rate:
-#             resample = Resample(sample_rate, self.target_sample_rate)
-#             audio = resample(audio)
-        
-#         if audio.shape[0] > self.num_samples:
-#             audio = self.crop_audio(audio)
-            
-#         if audio.shape[0] < self.num_samples:
-#             audio = self.pad_audio(audio)
-            
-#         mel_spectogram = MelSpectrogram(sample_rate=self.target_sample_rate, 
-#                                         n_mels=CONFIG['n_mels'], 
-#                                         n_fft=CONFIG['n_fft'])
-#         mel = mel_spectogram(audio)
-        
-#         label_onehot = torch.zeros(CONFIG['num_classes'])
-#         label_onehot[self.labels[index]] = 1
-#         label = torch.tensor(self.labels[index])
-        
-#         # Convert to Image
-#         image = torch.stack([mel, mel, mel])
-        
-#         # Normalize Image
-#         max_val = torch.abs(image).max()
-#         image = image / max_val
-        
-#         return {
-#             "image": image, 
-#             "label": label,
-#             "label_onehot": label_onehot
-#         }
-            
-#     def pad_audio(self, audio):
-#         pad_length = self.num_samples - audio.shape[0]
-#         last_dim_padding = (0, pad_length)
-#         audio = F.pad(audio, last_dim_padding)
-#         return audio
-        
-#     def crop_audio(self, audio):
-#         return audio[:self.num_samples]
-        
-#     def to_mono(self, audio):
-#         return torch.mean(audio, axis=0)
